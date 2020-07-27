a4d8ab8511521330b86f800370d75959c3318fdc
nomelancholy <starmekey@naver.com>
2020-07-27 18:13:42 +0900

    add dirver switch to popup feature

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const {Builder, BY, Key, util} = require('selenium-webdriver');
+const { Builder, By, Key, until } = require("selenium-webdriver");
 
-async function example() {
-    let driver = await new Builder().forBrowser('chrome').build();
-    await driver.get('http://google.com');
+async function ticketlink() {
+  let driver = await new Builder().forBrowser("chrome").build();
+  try {
+    await driver.get("http://www.ticketlink.co.kr/");
+
+    const homepage = await driver.getWindowHandle();
+
+    await driver.findElement(By.id('loginBtn')).click();
+
+    let windows = await driver.getAllWindowHandles();
+    
+    for (const window of windows) {
+      if(window !== homepage){
+        driver.switchTo().window(window)
+        break;
+      }
+    }
+    
+    await driver.findElement(By.id('id')).sendKeys('abcd', Key.RETURN);
+
+    // console.log('windows[0]', windows[0])
+
+    // await driver.wait(until.ableToSwitchToFrame())
+    console.log('here')
+    // driver.switchTo().window('window')
+
+    // await driver.findElement(By.id('id')).sendKeys('abcd', Key.RETURN);
+
+  } catch (error) {
+      console.log('error', error)
+  } finally {
+    // await driver.quit();
+  }
 }
 
-example();
+ticketlink();
