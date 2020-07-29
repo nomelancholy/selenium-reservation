b45b8c39712f53d4f9b5dec38561ea7a1afc7f20
nomelancholy <starmekey@naver.com>
2020-07-29 13:23:04 +0900

    reservation success

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 async function ticketlink() {
   let driver = await new Builder().forBrowser("chrome").build();
   try {
-    // await driver.get("http://www.ticketlink.co.kr/sports/football/77");
-    await driver.get("http://www.ticketlink.co.kr/sports/baseball/57");
+    await driver.get("http://www.ticketlink.co.kr/sports/football/77");
+    // await driver.get("http://www.ticketlink.co.kr/sports/baseball/57");
 
     // click login btn
     const homepage = await driver.getWindowHandle();
@@ -33,8 +33,10 @@ async function ticketlink() {
     await driver.switchTo().window(homepage);
 
     // click reservation button
-    elements = await driver.findElements(By.className("btn_reserve"));
-    await elements[0].click();
+    let reserveStartButtons = await driver.findElements(
+      By.className("btn_reserve")
+    );
+    await reserveStartButtons[0].click();
     await driver.findElement(By.id("noticeModalClose")).click();
 
     // driver switch to popup
@@ -49,25 +51,48 @@ async function ticketlink() {
 
     // 좌석 메뉴 클릭
     await driver
-      .findElement(By.xpath("//*[text()[contains(.,'VIP석')]]"))
+      .findElement(By.xpath("//*[text()[contains(.,'S 지정석')]]"))
       .click();
     await driver
-      .findElement(By.xpath("//*[text()[contains(.,'VIP2구역')]]"))
+      .findElement(By.xpath("//*[text()[contains(.,'S 4구역')]]"))
       .click();
-
-    // await driver
-    //   .findElement(By.xpath("//*[text()[contains(.,'S석')]]"))
-    //   .click();
-    // await driver
-    //   .findElement(By.xpath("//*[text()[contains(.,'VIP2구역')]]"))
-    //   .click();
 
     // 좌석 클릭할 때 까지 기다렸다 진행
 
-    await driver.wait(until.elementLocated(By.className("reserve_next2")));
-    await driver.findElement(By.className('reserve_next2')).click();
+    await driver.wait(until.alertIsPresent());
 
+    await driver.switchTo().alert().accept();
 
+    await driver.switchTo(homepage);
+
+    // await driver.wait(until.elementLocated(By.className("reserve_next2")));
+
+    await driver.findElement(By.className("reserve_next2")).click();
+
+    // 권종/할인/매수 선택
+
+    await driver.wait(until.elementLocated(By.className("selectbox")));
+
+    let selectboxes = await driver.findElements(By.className("selectbox"));
+    await selectboxes[0].click();
+
+    await driver
+      .findElement(By.css('li[data-sale_price="12000"][data-value="1"]'))
+      .click();
+
+    await driver.findElement(By.id("reserveNext")).click();
+
+    // 배송선택/예매확인
+
+    await driver.wait(until.elementsLocated(By.className("checkbox")));
+
+    let checkboxes = await driver.findElements(By.className("checkbox"));
+
+    for (const checkbox of checkboxes) {
+      checkbox.click();
+    }
+
+    await driver.findElement(By.className("reserve_pay4")).click();
   } catch (error) {
     console.log("error", error);
   } finally {
